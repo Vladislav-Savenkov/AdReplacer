c0cb8b4ed2c5f9c376c20787df2515a852da50ce
ISich <arseny.voroni@yandex.ru>
2023-11-28 19:51:05 +0500

    Add class

--- PhotoRedactor/Redactor.py
+++ PhotoRedactor/Redactor.py
@@ -1,17 +1,13 @@
 from PIL import Image
+from Photo import Photo
 
 
 def main():
-    filename1 = "test_photo1.jpg"
-    filename2 = "test_photo2.jpg"
-    with Image.open(filename1) as cat:
-        cat.load()
-    with Image.open(filename2) as gosl:
-        gosl.load()
-    dx, dy = 80, 50
-    c_w, c_h, g_w, g_h = cat.width, cat.height, gosl.width, gosl.height
-    gosl.paste(cat.resize((g_w - dx * 2, g_h - dy * 2)), (dx, dy))
-    gosl.show()
+    photo1 = Photo("test_photo1.jpg")
+    photo2 = Photo("test_photo2.jpg")
+    dx, dy = 100, 100
+    photo1.paste(photo2, dx, dy)
+    photo1.photo.show()
 
 
 if __name__ == "__main__":
